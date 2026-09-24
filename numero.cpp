75538366e03e81e9fafd876a8142f553762f403c
Julissa-Cooper <00078526@uca.edu.sv>
2026-09-24 15:08:15 -0600

    subiendo archivo

---
+++ numero.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+ int main ()  {
+      float numero;
+
+      cout << " Ingrese un numero " << endl;
+    cin >> numero;
+
+    if  (numero > 0) {
+         cout << "el numero es positivo"<< endl;
+
+    } else {
+        cout << "el numero es negativo "<< endl;
+    }
+
+return 0;
+ }
